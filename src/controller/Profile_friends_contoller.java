2ab818a587ca74888eaf0078124469097777291c
mamulee <ghdah7@gmail.com>
2021-01-13 18:06:20 +0900

    PLZZZZ

--- src/controller/Profile_friends_contoller.java
+++ src/controller/Profile_friends_contoller.java
@@ -16,61 +16,53 @@ import javafx.stage.Stage;
 import model.UserDTO;
 
 public class Profile_friends_contoller implements Initializable{
-	
-
-	@FXML private ImageView profile_friends_chat_label;
-	@FXML private ImageView profile_friends_exit;
-	@FXML private Label profile_friends_chat_label;
-	@FXML private Label friend_name;
-	@FXML private Label friend_status;
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		
-		friend_name.setText(UserDTO.withFriend.getName());
-		friend_status.setText(UserDTO.withFriend.getStatus());
-		
-		profile_friends_chat_label.setOnMousePressed(new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent event) {
-				if(event.getButton() == MouseButton.PRIMARY) {
-					try {
-						UserDAO dao = new UserDAO();
-						int room_num = dao.roomCheck(UserDTO.nowUser, UserDTO.withFriend);
-						Chat_w_01_controller.room_num = room_num;
-						
-						Parent signup=FXMLLoader.load(getClass().getClassLoader().getResource("view/Chat_w_01.fxml"));
-						Scene scene = new Scene(signup);
-						Stage primaryStage = (Stage) profile_friends_chat_label.getScene().getWindow();
-						primaryStage.setTitle("Chatting");
-						primaryStage.setScene(scene);
-					}catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			};
-		});
-		
-		profile_friends_exit.setOnMousePressed(new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent event) {
-				if(event.getButton() == MouseButton.PRIMARY) {
-					try {
-							
-						Parent signup=FXMLLoader.load(getClass().getClassLoader().getResource("view/Friends"));
-						Scene scene = new Scene(signup);
-						Stage primaryStage = (Stage) profile_friends_exit.getScene().getWindow();
-						primaryStage.setTitle("Chatting");
-						primaryStage.setScene(scene);
-					}catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			};
-		});
-		
-	}
-	
-	
-	
-	
-	
+   
+   @FXML private ImageView profile_friends_chat_label;
+   @FXML private ImageView profile_friends_exit;
+   
+   @Override
+   public void initialize(URL location, ResourceBundle resources) {
+      profile_friends_chat_label.setOnMousePressed(new EventHandler<MouseEvent>() {
+         public void handle(MouseEvent event) {
+            if(event.getButton() == MouseButton.PRIMARY) {
+               try {
+                  UserDAO dao = new UserDAO();
+                  int room_num = dao.roomCheck(UserDTO.nowUser, UserDTO.withFriend);
+                  Chat_w_01_controller.room_num = room_num;
+                  
+                  Parent signup=FXMLLoader.load(getClass().getClassLoader().getResource("view/Chat_w_01.fxml"));
+                  Scene scene = new Scene(signup);
+                  Stage primaryStage = (Stage) profile_friends_chat_label.getScene().getWindow();
+                  primaryStage.setTitle("Chatting");
+                  primaryStage.setScene(scene);
+               }catch (Exception e) {
+                  e.printStackTrace();
+               }
+            }
+         };
+      });
+      
+      profile_friends_exit.setOnMousePressed(new EventHandler<MouseEvent>() {
+         public void handle(MouseEvent event) {
+            if(event.getButton() == MouseButton.PRIMARY) {
+               try {
+                     
+                  Parent signup=FXMLLoader.load(getClass().getClassLoader().getResource("view/Friends"));
+                  Scene scene = new Scene(signup);
+                  Stage primaryStage = (Stage) profile_friends_exit.getScene().getWindow();
+                  primaryStage.setTitle("Chatting");
+                  primaryStage.setScene(scene);
+               }catch (Exception e) {
+                  e.printStackTrace();
+               }
+            }
+         };
+      });
+      
+   }
+   
+   
+   
+   
+   
 }
